27eaa3201ca59ef2467e8928c1741efb2bb5a403
CetO-hub <skynet@SkyNet.localdomain>
2022-05-04 19:07:12 +0200

    Update script for generating random computer item

---
+++ script.js
@@ -0,0 +1,6 @@
+function generateItemComputer() {
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  if (randomNumber === 1) return "rock";
+  else if (randomNumber === 2) return "paper";
+  else return "scissors";
+}
