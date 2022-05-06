5ab711caaac7c389d73d7da945cf09b88267ff2c
CetO-hub <skynet@SkyNet.localdomain>
2022-05-06 14:51:07 +0200

    Update script display winner and reset board
    Add function for displaying the winner and add a function to reset
    the board for the next round.

--- script.js
+++ script.js
@@ -13,20 +13,62 @@ function selectPlayer(e) {
 }
 
 // Play one round
-function playRound(player, computer) {
-  console.log(player);
-  console.log(computer);
-  return;
+function playRound(playerItem, computerItem) {
+  if (
+    (playerItem === "rock" && computerItem === "scissors") ||
+    (playerItem === "scissors" && computerItem === "paper") ||
+    (playerItem === "paper" && computerItem === "rock")
+  ) {
+    return displayWinnerRound("player");
+  }
+  return displayWinnerRound("computer");
 }
 
 // Display the winner
-function displayWinnerRound() {
+function displayWinnerRound(winner) {
+  console.log(winner);
+  if (winner === "player") {
+    playerCount++;
+    let won = document.querySelector(".scorePlayer");
+    let wonMessage = document.querySelector(".won");
+    let makeChoice = document.querySelector(".makeChoice");
+    let arrows = document.querySelectorAll(".arrow");
+    wonMessage.setAttribute("style", "display:flex;");
+    won.innerText = playerCount;
+    makeChoice.setAttribute("style", "display:none;");
+    arrows.forEach((arrow) => arrow.setAttribute("style", "display:none;"));
+    return setTimeout(resetBoard, 1000);
+  }
+  computerCount++;
+  let won = document.querySelector(".scoreComputer");
+  let lostMessage = document.querySelector(".lost");
+  let makeChoice = document.querySelector(".makeChoice");
+  let arrows = document.querySelectorAll(".arrow");
+  lostMessage.setAttribute("style", "display:flex;");
+  won.innerText = computerCount;
+  makeChoice.setAttribute("style", "display:none;");
+  arrows.forEach((arrow) => arrow.setAttribute("style", "display:none;"));
+  return setTimeout(resetBoard, 1000);
+}
+
+// Reset the styles of the board for next round
+function resetBoard() {
+  let makeChoice = document.querySelector(".makeChoice");
+  let arrows = document.querySelectorAll(".arrow");
+  makeChoice.setAttribute("style", "display:flex;");
+  arrows.forEach((arrow) => arrow.setAttribute("style", "visibility:visible;"));
+  let lostMessage = document.querySelector(".lost");
+  let wonMessage = document.querySelector(".won");
+  lostMessage.setAttribute("style", "display:none;");
+  wonMessage.setAttribute("style", "display:none;");
   return;
 }
-// DIsplay the winner of the game
+// Display the winner of the game
 function displayWinnerGame() {
   return;
 }
+let playerCount = 0;
+let computerCount = 0;
 
 let isButtons = document.querySelectorAll(".btn");
 isButtons.forEach((btn) => btn.addEventListener("click", selectPlayer));
